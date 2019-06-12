7749e04813944aaf3f41803405346f864b40c3cb
Mark Halonen <halonen.mark@gmail.com>
2019-06-12 12:57:07 -0400

    add remaining functionality

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
-console.log('here!!');
 var numColumns = 3;
-var spreadsheet = document.getElementById("spreadsheet");
-console.log(spreadsheet);
+var numRows = 2;
+var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXZY';
+document.getElementById("addRowButton").onclick = function (e) {
+    numRows = numRows + 1;
+    render();
+};
+document.getElementById("addColumnButton").onclick = function (e) {
+    numColumns = numColumns + 1;
+    render();
+};
+function render() {
+    var spreadsheet = document.getElementById("spreadsheet");
+    spreadsheet.style.gridTemplateColumns = Array(numColumns).fill(0).map(function (_) { return '200px'; }).join(' ');
+    var html = Array(numColumns * numRows).fill(0).map(function (_, idx) { return "<input class=\"box a\" id=cell_" + idx + "></input>"; }).join('');
+    spreadsheet.innerHTML = html;
+    Array(numColumns * numRows).fill(0).forEach(function (_, idx) {
+        var cell = document.getElementById("cell_" + idx);
+        cell.addEventListener("keyup", function (event) {
+            event.preventDefault();
+            if (event.keyCode === 13) {
+                if (cell.value.startsWith("=")) {
+                    var calculationInput = cell.value.slice(1);
+                    // de-reference A1, B1, C1 etc
+                    calculationInput = calculationInput.split(' ').join('');
+                    calculationInput = calculationInput.split('+').map(function (el) {
+                        var letterIndex = letters.indexOf(el.charAt(0));
+                        var row = parseInt(el.charAt(1));
+                        if (letterIndex > -1) {
+                            var cellIdx = (row - 1) * numColumns + letterIndex;
+                            console.log(cellIdx);
+                            return document.getElementById("cell_" + cellIdx).value;
+                        }
+                        return el;
+                    }).join('+');
+                    cell.value = eval(calculationInput);
+                    cell.blur();
+                }
+            }
+        });
+    });
+}
+render();
--- index.ts
+++ index.ts
@@ -1,8 +1,53 @@
-console.log('here!!')
-
 let numColumns = 3
+let numRows = 2
+let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXZY'
 
-let spreadsheet = document.getElementById("spreadsheet")
+document.getElementById("addRowButton").onclick = e => {
+    numRows = numRows + 1
+    render()
+}
 
+document.getElementById("addColumnButton").onclick = e => {
+    numColumns = numColumns + 1
+    render()
+}
 
-console.log(spreadsheet)
+function render() {
+
+    let spreadsheet = document.getElementById("spreadsheet")
+
+    spreadsheet.style.gridTemplateColumns = Array(numColumns).fill(0).map(_ => '200px').join(' ')
+
+    let html = Array(numColumns * numRows).fill(0).map((_, idx) => `<input class="box a" id=cell_${idx}></input>`).join('')
+    spreadsheet.innerHTML = html
+
+    Array(numColumns * numRows).fill(0).forEach((_, idx) => {
+        let cell = document.getElementById(`cell_${idx}`) as HTMLInputElement
+
+        cell.addEventListener("keyup", function (event) {
+            event.preventDefault();
+            if (event.keyCode === 13) {
+                if (cell.value.startsWith("=")) {
+                    let calculationInput = cell.value.slice(1)
+
+                    // de-reference A1, B1, C1 etc
+                    calculationInput = calculationInput.split(' ').join('')
+                    calculationInput = calculationInput.split('+').map(el => {
+                        let letterIndex = letters.indexOf(el.charAt(0))
+                        let row = parseInt(el.charAt(1))
+                        if (letterIndex > -1) {
+                            let cellIdx = (row - 1) * numColumns + letterIndex
+                            console.log(cellIdx)
+                            return (document.getElementById(`cell_${cellIdx}`) as HTMLInputElement).value
+                        }
+                        return el
+                    }).join('+')
+                    cell.value = eval(calculationInput)
+                    cell.blur()
+                }
+            }
+        });
+    })
+}
+
+render()
