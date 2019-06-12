499b85086b72b016b1509300d9a3bf268ff25945
Mark Halonen <halonen.mark@gmail.com>
2019-06-12 12:05:51 -0400

    getting started

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log('here!!');
+var numColumns = 3;
+var spreadsheet = document.getElementById("spreadsheet");
+console.log(spreadsheet);
---
+++ index.ts
@@ -0,0 +1,8 @@
+console.log('here!!')
+
+let numColumns = 3
+
+let spreadsheet = document.getElementById("spreadsheet")
+
+
+console.log(spreadsheet)
